4cda566aed92a00d400021935909833e83bbea21
Maleesha Kawshan <kawshanmaleesha0@gmail.com>
2023-03-26 08:14:40 +0530

    create ui

--- QuickCurrency/ContentView.swift
+++ QuickCurrency/ContentView.swift
@@ -11,15 +11,75 @@ import SwiftUI
 struct ContentView: View {
     @State var input = "100"
     @State var base = "USD"
+    @State var currencyList = [String]()
+    @FocusState private var inputIsFoused: Bool
+    
+    func makeRquest(showAll: Bool, currencies: [String] = ["USD", "GBP", "EUR"]){
+        apiRequest(url: "https://api.exchangerate.host/latest?base=\(base)&amount=\(input)") { currency in
+            var tempList = [String]()
+            
+            for currency in currency.rates {
+                if showAll {
+                    tempList.append("\(currency.key) \(String(format: "%.2f", currency.value))")
+                }else if currencies.contains(currency.key){
+                    tempList.append("\(currency.key) \(String(format: "%.2f", currency.value))")
+                }
+                tempList.sort()
+            }
+            currencyList.self = tempList
+            print(tempList)
+        }
+    }
     
     var body: some View {
-        Text("hello world!")
-        .padding()
-        .onAppear {
-            apiRequest(url: "https://api.exchangerate.host/latest?base=\(base)&amount=\(input)") { currency
-                in
-                //print(currency)
+        VStack {
+            HStack {
+                Text("QuickCurrency")
+                    .font(.system(size: 30))
+                    .bold()
+                Image(systemName: "eurosign.circle.fill")
+                    .font(.system(size: 30))
+                    .foregroundColor(.purple)
             }
+            List{
+                ForEach(currencyList, id: \.self) { Currency in
+                    Text(Currency)
+                }
+            }
+            VStack{
+                Rectangle()
+                    .frame(height: 8.0)
+                    .foregroundColor(.purple)
+                    .opacity(0.90)
+                TextField("Enter an amount", text: $input)
+                    .padding()
+                    .background(Color.gray.opacity(0.10))
+                    .cornerRadius(20.0)
+                    .padding()
+                    .keyboardType(.decimalPad)
+                    .focused($inputIsFoused)
+                
+                TextField("Enter a currency", text: $base)
+                    .padding()
+                    .background(Color.gray.opacity(0.10))
+                    .cornerRadius(20.0)
+                    .padding()
+                    .focused($inputIsFoused)
+                
+                Button("convert") {
+                    makeRquest(showAll: true, currencies: ["LKR", "SEK", "NOK"])
+                    inputIsFoused = false
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.purple)
+                .cornerRadius(10)
+                .padding()
+                
+            }
+            
+        }.onAppear{
+            makeRquest(showAll: true)
         }
     }
 }
